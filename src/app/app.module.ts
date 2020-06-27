2de428a33577c4af2219e17fbe6b440a8537596f
IvanSaRo <ivansancrodr@gmail.com>
2020-06-27 15:47:57 +0200

    Creado proyecto error peer dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MaterialModule} from './material.module';
+import { MapsComponent } from './components/maps/maps.component'
+
+
+import { GoogleMapsModule } from '@angular/google-maps'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    GoogleMapsModule
+
+  ],
+  exports: [
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
